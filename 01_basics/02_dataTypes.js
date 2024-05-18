99f89ffa8cb1d7beb2226a397b2d9e7731db4811
RAVI KUMAR CHOUDHARY <91760318+ravi00010@users.noreply.github.com>
2024-05-18 16:59:52 +0000

    variables & datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"user strict"; //treat all JS code as newer vwesion
+
+// alert(3+3) //we are using nodeJs, not browser
+
+console.log("RAvi")
+
+let name="Ravi"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number=>2 to power 53
+// bigint
+// string => """
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
