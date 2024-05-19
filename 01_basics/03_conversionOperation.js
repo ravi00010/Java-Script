789b9d6e8e48a7926fa163ddf868780a7df4488d
RAVI KUMAR CHOUDHARY <91760318+ravi00010@users.noreply.github.com>
2024-05-19 13:27:10 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,44 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ******************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 =" ravi"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+
+console.log( (3+4) *5 % 3);
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+
+let num1,num2,num3
+
+num1=num2=num3= 2 + 2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter); //101
